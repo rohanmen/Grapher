070f1984702335e913ef9c2c253b17a714883eee
gonnapwnu <rohanmen@yahoo.com>
2016-06-18 23:04:01 -0500

    fixed client.py

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import os
 import json
 import socket
+import random
 
 
 def sendMessage(ip, port, message):
@@ -9,11 +10,14 @@ def sendMessage(ip, port, message):
 	request_sender.sendall(message)
 	request_sender.close()
 
-with open("config.json") as json_file:
-	server_data = json.load(json_file)
+#with open("config.json") as json_file:
+#	server_data = json.load(json_file)
 
-ip = server_data["ip"]
-port = server_data["port"]
+#ip = server_data["ip"]
+#port = server_data["port"]
+
+ip = ""
+port = 1100
 
 print ip, port
 
@@ -21,16 +25,17 @@ print ip, port
 #request_sender.connect((ip, port))
 #request_sender.sendall("hi this is a test")
 
+x = 0
 while True:
 	#text = raw_input()
 
-	i = 0
-	while i < 100:
-		sendMessage(ip, port, str(i))
-		i = i + 1
-	text = raw_input()
-	#if(text == 'exit'):
-		#break
+	y = random.randint(0,100)
+	val = str(x) + str(y)
+
+	print val
+
+	sendMessage(ip, port, val)
+	x = x + 1
 	
 
 
